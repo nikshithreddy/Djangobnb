bd7d3f28a69fb0e77fa149601deca481d1dc607d
nikshithreddy <165375024+nikshithreddy@users.noreply.github.com>
2025-02-04 12:24:51 -0800

    added basic navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h2 className="text-airbnb">App </h2>
+      <div>
+        <h2 className="text-airbnb">App </h2>
+      </div>
     </main>
   );
 }
